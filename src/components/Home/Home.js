ff90801c0b99c4f0bfa5a1f6a83b9b19d27f7aa3
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2021-08-31 22:19:16 +0200

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="home">aaaaa</div>
+}
+
+export default Home
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import Home from '../Home/Home'
+
+import './Main.css'
+
+function Main() {
+  return (
+    <div className="main">
+      <Home />
+    </div>
+  )
+}
+
+export default Main
